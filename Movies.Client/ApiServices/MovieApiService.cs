c1456afe6151164f65655e2a8a6da3c0ea36895b
Matevos Grigoryan <matevos.grigoryan@technamin.com>
2022-09-28 16:22:07 +0400

    Add project files.

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,85 @@
+﻿using IdentityModel.Client;
+using Movies.Client.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var applicationCredentials = new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:6005/connect/token",
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+                Scope = "movieAPI"
+            };
+
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:6005");
+            if (disco.IsError)
+            {
+                return null;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(applicationCredentials);
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+            var apiClient = new HttpClient();
+
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:6001/api/movies");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+
+            return movieList;
+            //var movieList = new List<Movie>();
+            //movieList.Add(
+            //    new Movie
+            //    {
+            //        Id = 1,
+            //        Genre = "Comics",
+            //        Title = "asd",
+            //        Rating = "9.2",
+            //        ImageUrl = "images/src",
+            //        ReleaseData = System.DateTime.Now,
+            //        Owner = "swn"
+            //    }
+            //    );
+            //return await Task.FromResult(movieList);
+        }
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteMovie(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+       
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Movies.Client/Startup.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.Client.Data;
+using Movies.Client.ApiServices;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Movies.Client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(); // added
+
+
+            services.AddScoped<IMovieApiService, MovieApiService>();
+
+            //services.AddDbContext<MoviesClientContext>(options =>
+            //        options.UseSqlServer(Configuration.GetConnectionString("MoviesClientContext")));
+
+
+
+            services.AddAuthentication(options => 
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme,options => 
+            {
+                options.Authority = "https://localhost:6005";
+
+                options.ClientId = "movies_mvc_client";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+
+                options.SaveTokens = true;
+
+                options.GetClaimsFromUserInfoEndpoint = true;
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();// added
+            app.UseAuthorization();
+        
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
